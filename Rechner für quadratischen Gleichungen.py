5c1b7322af796fad343f90155f8eef550fbc2f05
kakashkin52 <pandaibjj@gmail.com>
2025-02-05 11:37:47 +0100

    ta ladno

--- Rechner für quadratischen Gleichungen.py
+++ Rechner für quadratischen Gleichungen.py
@@ -56,7 +56,7 @@ async def handle_message(update: Update, context: ContextTypes. DEFAULT_TYPE):
 def main():
     app = Application.builder(). token(TOKEN). build()
     app.add_handler(CommandHandler("start",start_command))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
 
 if __name__ == "__main__":
